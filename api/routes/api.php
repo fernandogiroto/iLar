350175d886e282ad21c559926f889e3928a82e1f
André Pinheiro <andre.pinheiro@doutorfinancas.pt>
2021-07-05 09:53:06 +0100

    adicionado ficheiro de routes api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,39 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::middleware(['cors', 'json.response', 'auth:api'])->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-
-Route::group(['middleware' => ['cors', 'json.response']], function () {
-
-    // ...
-
-    // public routes
-    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
-    Route::post('/register', 'Auth\ApiAuthController@register')->name('register.api');
-
-    // ...
-
-}); */
-
-
-
-
-
-/*
-Route::group(['middleware' => ['cors']], function () {
-    Route::apiResource('patients','PatientController');
-    Route::apiResource('users','UserController');
-
-    // public routes
-    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
-    Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
-    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
-
-});
-*/
  Route::group([
 
     'middleware' => ['api', 'cors'],
